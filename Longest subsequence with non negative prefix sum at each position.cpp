a909eb31fa4bcacc9527995003906718ec39e6a8
Karthickabi <43386448+Karthickabi@users.noreply.github.com>
2021-08-09 16:49:58 +0530

    Create Longest subsequence with non negative prefix sum at each position.cpp

---
+++ Longest subsequence with non negative prefix sum at each position.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+//prefix such that for ith element sum of 0 to ith element should be >=0
+void longestSubsequence(int* arr, int N)
+{
+	int dp[N][N + 1];
+	memset(dp, -1, sizeof dp);
+
+	for (int i = 0; i < N; ++i) 
+	{
+		dp[i][0] = 0;
+	}
+	dp[0][1] = (arr[0] >= 0 ? arr[0] : -1);
+
+	for (int i = 1; i < N; ++i) 
+	{
+	    for (int j = 1;j <= (i + 1); ++j) 
+	    {
+			if (dp[i - 1][j] != -1)
+			{
+				dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+			}
+			
+			if (dp[i - 1][j - 1] >= 0&& dp[i - 1][j - 1]+ arr[i]>= 0) 
+			{
+			    dp[i][j] = max(dp[i][j],dp[i - 1][j - 1]+ arr[i]);
+			}
+		}
+	}
+	int ans = 0;
+	for (int j = 0; j <= N; ++j) 
+	{
+		if (dp[N - 1][j] >= 0) 
+		{
+			ans = j;
+		}
+	}
+	
+	cout << ans << endl;
+}
+
+int main()
+{
+	int arr[] = { 4, -4, 1, -3, 1, -3 };
+	int N = sizeof arr / sizeof arr[0];
+	longestSubsequence(arr, N);
+	return 0;
+}
